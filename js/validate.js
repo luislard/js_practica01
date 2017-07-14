31ba1f19a3cde70e45a14301a823149f5c5b8303
Luis Rosales <eng.luisrosales@gmail.com>
2017-07-14 21:25:05 +0200

    fixing commented property in css grid, adding and creating validate js file

---
+++ js/validate.js
@@ -0,0 +1,22 @@
+// Validating input name
+
+// Not blank
+
+// Validating input email
+// Not blank
+// valid email
+
+// Validating how
+// Not blank
+// Proper other handling
+
+// Validating phone
+// not blank
+// validate number
+
+// Validating textarea
+// not blank
+// 150 words maximum
+
+
+
