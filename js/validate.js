64f7e18e1e55d261709a332bb06aacc1cc5c2a39
Luis Rosales <eng.luisrosales@gmail.com>
2017-07-14 23:13:31 +0200

    adding functions to validate not blank inputs and also a function to retrieve the value od the radio input

--- js/validate.js
+++ js/validate.js
@@ -1,4 +1,63 @@
+/**
+ *
+ * This function checks if the input or textarea is not blank,
+ * returns true when is not blank.
+ *
+ * @param input|textarea
+ * @returns {boolean}
+ */
+function isInputNotBlank(input){
+
+    var content = input.value;
+    if(content != ""){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+/**
+ * This function checks if the input radio of the contact form is not blank,
+ * returns true when is not blank.
+ * @returns {boolean}
+ */
+function isRadioNotBlank() {
+    var radioChecked = document.querySelector('input[name="how"]:checked');
+    if (radioChecked == null){
+        return false;
+    }else{
+        // check if other is selected
+        if(radioChecked.value == 'Other'){
+            // check if the input is filled
+            var inputOther = document.getElementById('inputOther');
+            if(isInputNotBlank(inputOther)){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return true;
+        }
+    }
+}
+
+/**
+ * This function receives the inputRadio already checked and returns the value
+ * @param radioChecked
+ * @returns {Number|string}
+ */
+function getRadioValue(radioChecked) {
+
+    if (radioChecked.value != 'Other') {
+        return radioChecked.value;
+    } else {
+        return  document.getElementById('inputOther').value;
+    }
+}
+
+
 // Validating input name
+
 
 // Not blank
 
