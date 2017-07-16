df9166872f3914475c822ea3296bd29ac7658272
Luis Rosales <eng.luisrosales@gmail.com>
2017-07-16 14:46:18 +0200

    adding form validation

--- js/validate.js
+++ js/validate.js
@@ -1,4 +1,11 @@
 /**
+ * FUNCTIONS
+ */
+
+
+
+
+/**
  *
  * This function checks if the input or textarea is not blank,
  * returns true when is not blank.
@@ -46,12 +53,17 @@ function isRadioNotBlank() {
  * @param radioChecked
  * @returns {Number|string}
  */
-function getRadioValue(radioChecked) {
+function getRadioValue() {
+    var radioChecked = document.querySelector('input[name="how"]:checked');
+    if (radioChecked != null){
 
-    if (radioChecked.value != 'Other') {
-        return radioChecked.value;
-    } else {
-        return  document.getElementById('inputOther').value;
+        if (radioChecked.value != 'Other') {
+            return radioChecked.value;
+        } else {
+            return  document.getElementById('inputOther').value;
+        }
+    }else{
+        return 'no radio selected';
     }
 }
 
@@ -84,27 +96,176 @@ function checkPhoneFormat(inputPhone){
     }
 }
 
+/**
+ * This function counts the words of a string.
+ * @param str
+ * @returns {Number}
+ */
+function countWords(str){
+    // normalize spaces
+    str = str.replace(/\s+/gm," ");
+    // deleting start and end spaces
+    str = str.trim();
+    var arr = str.split(" ");
+    var count = arr.length;
+    if (arr[0] === ''){
+        return 0;
+    }else{
+        return count;
+    }
+}
+/**
+ * This functions checks if the element has a given class class
+ * @param element
+ * @param cls
+ * @returns {boolean}
+ */
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
 
-// Validating input name
+
+function checkIfRadioOtherWasSelectedAndRevealTheInput(){
+    console.log('im in');
+    var radioChecked = document.querySelector('input[name="how"]:checked');
+    if (radioChecked.value === 'Other') {
+        inputOther.classList.remove('hidden');
+    }else{
+        inputOther.classList.add('hidden');
+    }
+}
+
+function isFormValid(){
+    var noError = true;
+    if (isInputNotBlank(inputName) === false){
+        spanName.innerText = 'This field should not be blank.';
+        spanName.classList.remove('no-visibility');
+        noError = false;
+    }else{
+        spanName.classList.add('no-visibility');
+        noError = true;
+    }
+
+    if (isInputNotBlank(inputPhone) === false){
+        spanPhone.innerText = 'This field should not be blank.';
+        spanPhone.classList.remove('no-visibility');
+        noError = false;
+    }else if(checkPhoneFormat(inputPhone) === false) {
+        spanPhone.innerText = 'This is not a valid phone number. Hint: +34610123456.';
+        spanPhone.classList.remove('no-visibility');
+        noError = false;
+
+    }else{
+
+        spanPhone.classList.add('no-visibility');
+        noError = true;
+    }
+
+    if (isInputNotBlank(inputEmail) === false){
+        spanEmail.innerText = 'This field should not be blank.';
+        spanEmail.classList.remove('no-visibility');
+        noError = false;
+    }else if (checkEmailFormat(inputEmail) === false){
+        spanEmail.innerText = 'This is not a valid email.';
+        spanEmail.classList.remove('no-visibility');
+        noError = false;
+    }else{
+
+        spanEmail.classList.add('no-visibility');
+        noError = true;
+    }
+
+    if (isInputNotBlank(inputMsg) === false){
+        spanMsg.innerText = 'This field should not be blank.';
+        spanMsg.classList.remove('no-visibility');
+        noError = false;
+    }else if (countWords(inputMsg.value) > 150){
+        spanMsg.innerText = '150 word max.';
+        spanMsg.classList.remove('no-visibility');
+        noError = false;
+    }else{
+        spanMsg.classList.add('no-visibility');
+        noError = true;
+
+    }
+
+    if (isRadioNotBlank() === false){
+        spanHow.innerText = 'This field should not be blank.';
+        spanHow.classList.remove('no-visibility');
+        noError = false;
+    }else{
+        spanHow.classList.add('no-visibility');
+        noError = true;
+    }
+
+    var wordsLeft = document.getElementById('words-left');
+
+    if (150 - countWords(inputMsg.value) > 0){
+
+        wordsLeft.innerText = 150 - countWords(inputMsg.value);
+    }else{
+        wordsLeft.innerText = 0;
+
+    }
+
+    if (noError) {
+        return true;
+    }else{
+        return false;
+    }
+}
 
 
-// Not blank
+/**
+ * JS CODE
+ */
 
-// Validating input email
-// Not blank
-// valid email
+var form = document.getElementById('contact-form');
+var send = document.getElementById('send');
 
-// Validating how
-// Not blank
-// Proper other handling
+var inputName = document.getElementById('name');
+var inputPhone = document.getElementById('phone');
+var inputEmail = document.getElementById('email');
+var inputMsg = document.getElementById('message');
+var spanName = document.getElementById('name-error');
+var spanPhone = document.getElementById('phone-error');
+var spanEmail = document.getElementById('email-error');
+var spanHow = document.getElementById('how-error');
+var spanMsg = document.getElementById('message-error');
+var inputOther = document.getElementById('inputOther');
 
-// Validating phone
-// not blank
-// validate number
+form.addEventListener('keyup', function (event) {
 
-// Validating textarea
-// not blank
-// 150 words maximum
+    isFormValid();
+
+});
+
+var howElements = document.querySelectorAll('input[name="how"]');
+console.log(howElements);
+howElements.forEach(function (element) {
+    console.log('en foreach')
+    element.addEventListener('click',function(){
+        isFormValid();
+        checkIfRadioOtherWasSelectedAndRevealTheInput();
+    });
+});
+
+send.addEventListener('click',function (e) {
+    if (isFormValid()){
+        alert('valido');
+        // form.submit();
+    }else{
+        e.preventDefault();
+        alert('no valido');
+
+    }
+});
+
+
+
+
+
+
 
 
 
